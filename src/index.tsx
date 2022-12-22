34af5e334789991d519a77481e1e349ce9becd62
mbarnesdev <martinbarnes.dev@gmail.com>
2022-12-22 01:49:30 +0000

    added react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root') as HTMLDivElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
