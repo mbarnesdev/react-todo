530b10bdbde902cb61ee4a247bb168b393ffcb63
mbarnesdev <martinbarnes.dev@gmail.com>
2022-12-22 21:01:14 +0000

    jotai atom example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { useState } from "react"
-import Counter from "./components/Counter/Counter"
+import { useAtom } from "jotai"
+import { isDarkModeAtom } from "./atoms"
+import cx from "classnames"
 
 const App = () => {
-  return <Counter />
+  const [isDarkMode, setIsDarkMode] = useAtom(isDarkModeAtom)
+
+  const handleToggleButton = () => setIsDarkMode((prevState: boolean) => !prevState)
+  const headerClassnames = cx({"dark-mode": isDarkMode})
+
+  return (
+    <div>
+      <h1 className={headerClassnames}>
+        Theme Color
+      </h1>
+      <button onClick={handleToggleButton}>
+        Toggle
+      </button>
+    </div>
+  )
 };
 
 export default App;
--- src/services/fetchTodos.ts
+++
@@ -1,9 +0,0 @@
-type Todo = {
-  id: string,
-  content: string,
-  completed: boolean,
-}
-
-export const fetchTodos = (): Promise<Todo[]> => {
-    return fetch("http://localhost:3005/todos").then((res) => res.json())
-}
